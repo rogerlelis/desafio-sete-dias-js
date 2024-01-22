fccf80faf8a65cdba819227be832306cc0280cef
Roger Lelis <huliseng@gmail.com>
2024-01-22 14:13:40 -0300

    Primeiro dia

--- dia-um.js
+++ dia-um.js
@@ -20,5 +20,5 @@ if (numeroTrinta == stringTrinta) {
 if (numeroDez == stringDez) {
   console.log("As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes");
 } else {
-  console.log("As variáveis numeroDez e stringDez não têm o mesmo valor");
+  console.log("As variáveis numeroDez e stringDez não têm o mesmo valor!");
 } */
